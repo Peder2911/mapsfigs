24950e9250e60ffd9e8d195078b0d426cd0fbbde
peder2911 <pglandsverk@gmail.com>
2020-10-07 14:15:15 +0200

    Fixed table!
    Proper chi square.
    
    Also has annotations

--- tab/tab.py
+++ tab/tab.py
@@ -9,11 +9,11 @@ import textwrap
 import azure.functions as func 
 
 import pandas as pd
-from scipy.stats import chisquare
+from scipy.stats import chi2_contingency 
 import jinja2
 
 from __app__.plotting import plotbytes,calcwidth,wrapped,wrap # pylint: disable=import-error
-from __app__.orm import get_session,connect # pylint: disable=import-error
+from __app__.orm import get_session,connect,Variable # pylint: disable=import-error
 from __app__.orm.services import withmeta,getvar,getdescr,getdict # pylint: disable=import-error
 
 STYLE = """
@@ -55,18 +55,23 @@ def htmlCrossTable(ctab,aggfn):
             </tr>
         {% endfor %}
         <tr>
-            <td class="summary" colspan = 100>
+            <td class="summary">
                 N: {{ n }}
+            </td>
+            <td class="summary">
                 Chi: {{ cstat }}
-                ({{ cp }})
+            </td>
+            <td class="summary">
+                P: {{ cp }}
             </td>
         </tr>
     </table>
     """)
     n = ctab.values.sum().sum()
-    chi = chisquare(ctab.values,axis=None)
-    cstat,cp = chi.statistic,chi.pvalue
+    chi = chi2_contingency(ctab.values)
+    cstat,cp,*_ = chi 
     cstat = round(cstat,4)
+    cp = round(cp,5)
 
     ctab = aggfn(ctab)
     logging.info(ctab)
@@ -95,9 +100,16 @@ def getAggFn(kind):
         fn = functions["count"]
     return fn
 
-def fixIndices(df):
-    df.columns = df.columns.astype(int)
-    df.index = df.index.astype(int)
+def fixIndices(df,v1,v2):
+    with contextlib.closing(get_session()) as sess:
+        lookups = []
+        for v in v1,v2:
+            mappings = sess.query(Variable).filter(Variable.name == v).first().mappings
+            keyval = {mp.key:mp.value for mp in mappings}
+            lookups.append(keyval)
+        v1,v2 = lookups 
+    df.columns = [v2[v] for v in df.columns.astype(int)]
+    df.index = [v1[v] for v in df.index.astype(int)]
     return df
 
 def main(req: func.HttpRequest):
@@ -110,6 +122,6 @@ def main(req: func.HttpRequest):
     except KeyError:
         kind = "count"
     aggfn = getAggFn(kind) 
-    ctab = fixIndices(pd.crosstab(df[v1],df[v2]))
+    ctab = fixIndices(pd.crosstab(df[v1],df[v2]),v1,v2)
     ctab = htmlCrossTable(ctab,aggfn)
     return func.HttpResponse(STYLE+ctab,mimetype="text/html")
